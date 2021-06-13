2e754a1cb0675699e29dbf52a8e6e2bd62cc9dd3
Max Davitt <max@davitt.me>
2021-06-13 19:25:55 +0000

    Initial commit

---
+++ html.js
@@ -0,0 +1,6 @@
+import * as runtime from 'hastscript/html/jsx-runtime';
+import { render } from '@themaxdavitt/component-wrapper';
+
+export const Fragment = null;
+export const jsx = render(runtime.jsx);
+export const jsxs = render(runtime.jsxs);
---
+++ svg.js
@@ -0,0 +1,6 @@
+import * as runtime from 'hastscript/svg/jsx-runtime';
+import { render } from '@themaxdavitt/component-wrapper';
+
+export const Fragment = null;
+export const jsx = render(runtime.jsx);
+export const jsxs = render(runtime.jsxs);
